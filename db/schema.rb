3db3857e1ea73f2d4086179f4cb2090268dfcdd7
EC2 Default User <naoya.take10.01@gmail.com>
2021-11-07 04:45:27 +0000

    [Fnish]top-layout

--- db/schema.rb
+++ db/schema.rb
@@ -20,12 +20,12 @@ ActiveRecord::Schema.define(version: 2021_11_06_064538) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "name", default: "", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
