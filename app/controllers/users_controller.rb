fc97de7641b3d37d07fddaecd9bff73edbbfc1c7
EC2 Default User <naoya.take10.01@gmail.com>
2021-11-07 01:08:12 +0000

    [finish]21-layout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,10 @@
 class UsersController < ApplicationController
   def index
-      # @user = User.find(params[:id])
+      @user = User.find(params[:id])
   end
   
   def show
-    # @user = User.find(params[:id])   
+    @user = User.find(params[:id])   
   end
   
   def edit
